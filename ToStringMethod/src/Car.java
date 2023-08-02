8177b1c30b9d3161cc752f147f46cc3227954c12
Harvey Duperier <59696097+hduperier@users.noreply.github.com>
2023-08-02 15:41:57 -0400

    Add files via upload

---
+++ ToStringMethod/src/Car.java
@@ -0,0 +1,12 @@
+
+public class Car {
+	String make = "Ford";
+	String model = "Mustang";
+	String color = "red";
+	int year = 2019;
+
+	public String toString() {
+		String myString = make + "\n" + model + "\n" + color + "\n" + year;
+		return myString;
+	}
+}
---
+++ ToStringMethod/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// toString() - all objects inherit this method that returns a string that textually represents an object
+		Car myCar = new Car();
+		/*
+		System.out.println(myCar.make);
+		System.out.println(myCar.model);
+		System.out.println(myCar.color);
+		System.out.println(myCar.year);
+		*/
+		System.out.println(myCar);// address for where you can find car object uses toString
+		
+		System.out.println(myCar.toString());
+	}
+
+}

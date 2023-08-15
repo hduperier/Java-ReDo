947d02dfafae848e5d3edd6e77d0f49a1dc0110a
Harvey Duperier <59696097+hduperier@users.noreply.github.com>
2023-08-15 16:38:44 -0400

    Add files via upload

---
+++ MethodOverriding/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// method overriding - Declaring a method in a subclass which is already present in parent class. 
+		// done so the child class can have its own implementation
+		
+		Dog dog = new Dog();
+		
+		dog.speak();
+		
+		Animal animal = new Animal();
+		animal.speak();
+			
+	}
+
+}

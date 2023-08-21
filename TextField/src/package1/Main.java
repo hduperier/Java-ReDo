acb6f0dedd0e556741cefe9294a158fd0f2132d2
Harvey Duperier <59696097+hduperier@users.noreply.github.com>
2023-08-21 19:21:06 -0400

    Add files via upload

---
+++ TextField/src/package1/Main.java
@@ -0,0 +1,14 @@
+package package1;
+
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// JTextField = GUI textbox component that can be used to add, set, or get text
+		
+		MyFrame frame = new MyFrame();
+		
+	}
+
+}

d6b8b4f3a118ae6f64b1413553f1d11971f2d4b8
Harvey Duperier <59696097+hduperier@users.noreply.github.com>
2023-08-22 16:39:32 -0400

    Add files via upload

---
+++ JSliderGUI/src/package1/Main.java
@@ -0,0 +1,13 @@
+package package1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// JSlider = GUI component that lets user enter a value by using an adjustable sliding knob on a track
+		
+		SliderDemo sliderDemo = new SliderDemo();
+		
+	}
+
+}
